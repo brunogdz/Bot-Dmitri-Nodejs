26b482edb5617958bd22de733fd4079981a9568a
Bruno Gomes de Azevedo <brunog.vix@gmail.com>
2020-07-02 20:36:09 -0300

    help não funcionou

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,15 +1,11 @@
 const execute = (bot, msg, args) => {
-    let string = "\n";
+    let string = "==== AJUDA ====\n\n";
     bot.commands.forEach((command) => {
         if (command.help) {
             string += `**${process.env.PREFIX}${command.name}**: ${command.help}\n`;
         }
     });
-    const embed = new MessageEmbed()
-        .setTitle("Status do Corona Virus no Brasil")
-        .setDescription(`${string}`)
-    msg.channel.send(embed);
-    // return msg.channel.send(string);
+    return msg.channel.send(string);
 };
 
 module.exports = {
