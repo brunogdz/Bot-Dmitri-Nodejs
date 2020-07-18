17dd2fe143d29f37c95df42ceb57ff31b92ed2e1
Bruno Gomes de Azevedo <brunog.vix@gmail.com>
2020-07-17 21:32:56 -0300

    tirado o teste e adicionado o weather

---
+++ src/commands/weather.js
@@ -0,0 +1,76 @@
+const MessageEmbed = require("discord.js").MessageEmbed;
+
+const axios = require('axios');
+async function execute(bot, msg, args) {
+    const s = args.join(" ");
+    const dadosAPI = await buscaDados(s);
+    const embed = new MessageEmbed()
+        .setTitle(`Clima da cidade: ${dadosAPI.Cidade}`)
+        .setDescription("Mostrando algumas informações sobre o clima da cidade que você digitou. Lembrando que tem que colocar o Estado após a cidade, separando com virgula")
+        .addFields(
+            {name: `Data hoje:${dadosAPI.data1}`, value:"⠀"},
+            {name: `Maxima hoje:${dadosAPI.Max1}`, value: "⠀"},
+            {name: `Minima hoje:${dadosAPI.Min1}`, value: "⠀"},
+            {name: `Amanhã:${dadosAPI.data2}`, value: "⠀", inline: true},
+            {name: `Maxima amanhã:${dadosAPI.Max2}`, value: "⠀", inline: true},
+            {name: `Maxima amanhã:${dadosAPI.Min2}`, value: "⠀", inline: true},
+            {name: `Tempo está:${dadosAPI.Descricao}`, value: "⠀"},
+            {name: `Umidade: ${dadosAPI.Umidade}%`, value: "⠀"},
+            {name: `Vento: ${dadosAPI.Speed}`, value: "⠀"},
+            {name: `Sol nascendo às: ${dadosAPI.Nascer}`, value: "⠀"},
+            {name: `Sol nascendo às: ${dadosAPI.Por}`, value: "⠀", inline: true},
+            {name: `Horas agora: ${dadosAPI.Hora}`, value: "⠀"}
+            
+        )
+        .setThumbnail("")
+        msg.channel.send(embed)
+};
+
+const buscaDados = async (s) => {
+    try {
+        const citys = s;
+        const linkValid = `https://api.hgbrasil.com/weather?key=${process.env.WEATHER}`;
+        var url = `${linkValid}&city_name=${encodeURI(citys)}`;
+        var urllimitada = `https://api.hgbrasil.com/weather?array_limit=2&fields=only_results,temp,city_name=${encodeURI(citys)},forecast,max,min,date&key=${process.env.WEATHER}`;
+        // const resultado = await axios.get("https://api.hgbrasil.com/weather?key=393df20d")
+
+        const resultado = await axios.get(urllimitada)
+
+        const { temp: Temperatura, date: Data } = resultado.data
+        const { date: data1, max: Max1, min: Min1 } = resultado.data.forecast[0]
+        const { date: data2, max: Max2, min: Min2 } = resultado.data.forecast[1]
+        console.log(Temperatura, Data, data1, Max1, Min1, data2, Max2, Min2)
+
+        const Plus = await axios.get(url)
+
+        const { description: Descricao, currently: status, city: Cidade, humidity: Umidade, wind_speedy: Speed, sunrise: Nascer, sunset: Por, time: Hora } = Plus.data.results
+
+
+        return {
+            Temperatura,
+            Hora,
+            data1,
+            Max1,
+            Min1,
+            data2,
+            Max2,
+            Min2,
+            Descricao,
+            status,
+            Cidade,
+            Umidade,
+            Speed,
+            Nascer,
+            Por
+
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
+module.exports = {
+    name: "clima",
+    aliases: ["weather", "tempo"],
+    help: "Mostra o clima de uma cidade",
+    execute,
+};
