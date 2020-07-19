2b093d5efa4500cdf0c1d2d208f3e375fc9e3ce7
Bruno Gomes de Azevedo <brunog.vix@gmail.com>
2020-07-18 23:57:57 -0300

    weather alterado rsrs

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -8,22 +8,23 @@ async function execute(bot, msg, args) {
         .setTitle(`Clima da cidade: ${dadosAPI.Cidade}`)
         .setDescription("Mostrando algumas informações sobre o clima da cidade que você digitou. Lembrando que tem que colocar o Estado após a cidade, separando com virgula")
         .addFields(
-            {name: `Data hoje:${dadosAPI.data1}`, value:"⠀", inline: true},
-            {name: `Máxima hoje:${dadosAPI.Max1}`, value: "⠀", inline: true},
-            {name: `Mínima hoje:${dadosAPI.Min1}`, value: "⠀", inline: true},
-            {name: `Amanhã:${dadosAPI.data2}`, value: "⠀", inline: true},
-            {name: `Máxima amanhã:${dadosAPI.Max2}`, value: "⠀", inline: true},
-            {name: `Mínima amanhã:${dadosAPI.Min2}`, value: "⠀", inline: true},
-            {name: `Tempo está:${dadosAPI.Descricao}`, value: "⠀"},
-            {name: `Umidade: ${dadosAPI.Umidade}%`, value: "⠀"},
-            {name: `Vento: ${dadosAPI.Speed}`, value: "⠀"},
-            {name: `Sol nascendo às: ${dadosAPI.Nascer}`, value: "⠀"},
-            {name: `Sol se põe às: ${dadosAPI.Por}`, value: "⠀", inline: true},
-            {name: `Horas agora: ${dadosAPI.Hora}`, value: "⠀"}
-            
+            { name: `Temperatura: ${dadosAPI.Temperatura}°C`, value: "⠀" },
+            { name: `Data hoje:${dadosAPI.data1}`, value: "⠀", inline: true },
+            { name: `Maxima hoje:${dadosAPI.Max1}°C`, value: "⠀", inline: true },
+            { name: `Minima hoje:${dadosAPI.Min1}°C`, value: "⠀", inline: true },
+            { name: `Amanhã:${dadosAPI.data2}`, value: "⠀", inline: true },
+            { name: `Maxima amanhã:${dadosAPI.Max2}°C`, value: "⠀", inline: true },
+            { name: `Maxima amanhã:${dadosAPI.Min2}°C`, value: "⠀", inline: true },
+            { name: `Tempo está:${dadosAPI.Descricao}`, value: "⠀" },
+            { name: `Umidade: ${dadosAPI.Umidade}%`, value: "⠀" },
+            { name: `Vento: ${dadosAPI.Speed}`, value: "⠀" },
+            { name: `Sol nascendo às: ${dadosAPI.Nascer}`, value: "⠀" },
+            { name: `Sol nascendo às: ${dadosAPI.Por}`, value: "⠀", inline: true },
+            { name: `Horas agora: ${dadosAPI.Hora}`, value: "⠀" }
+
         )
         .setThumbnail("")
-        msg.channel.send(embed)
+    msg.channel.send(embed)
 };
 
 const buscaDados = async (s) => {
@@ -35,16 +36,18 @@ const buscaDados = async (s) => {
         // const resultado = await axios.get("https://api.hgbrasil.com/weather?key=393df20d")
 
         const resultado = await axios.get(urllimitada)
-
-        const { temp: Temperatura, date: Data } = resultado.data
-        const { date: data1, max: Max1, min: Min1 } = resultado.data.forecast[0]
-        const { date: data2, max: Max2, min: Min2 } = resultado.data.forecast[1]
-        console.log(Temperatura, Data, data1, Max1, Min1, data2, Max2, Min2)
-
         const Plus = await axios.get(url)
+        const { temp: Temperatura, date: Data } = resultado.data
+        const { date: data1, max: Max1, min: Min1 } = Plus.data.results.forecast[0]
+        const { date: data2, max: Max2, min: Min2 } = Plus.data.results.forecast[1]
+        console.log(Temperatura, Data, data1, Max1, Min1, data2, Max2, Min2)
+        console.log(resultado.data)
+
 
         const { description: Descricao, currently: status, city: Cidade, humidity: Umidade, wind_speedy: Speed, sunrise: Nascer, sunset: Por, time: Hora } = Plus.data.results
 
+        console.log(Plus.data)
+        console.log((Plus.data).results.forecast)
 
         return {
             Temperatura,
