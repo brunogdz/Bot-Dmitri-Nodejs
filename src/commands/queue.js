01d6936d837237597027835c47a3e20cd832cfdb
Bruno Gomes de Azevedo <brunog.vix@gmail.com>
2020-07-22 00:05:16 -0300

    queue implementado com páginas

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -2,31 +2,60 @@ const { Utils } = require("erela.js")
 const { RichEmbed } = require("discord.js")
 const MessageEmbed = require("discord.js").MessageEmbed;
 
-const execute = (bot, msg, args) => {
+async function execute(bot, msg, args) {
     const queue = bot.queues.get(msg.guild.id);
 
     if (!queue) {
         return msg.reply("Não existe nenhuma musica na fila seu animal");
-    }
-    let que = queue.songs;
-    let np = queue.songs[0];
-    let showQ = `Tocando agora\n▶️ ${np.title} \n\n 🎵--FILA--🎵 \n\n `;
-    for (var i = 1; i < que.length; i++) {
-        showQ += `${i}) ${queue.songs[i].title}\n`;
-    }
-    // msg.channel.send(${np}\n\n);
-    msg.channel.send(showQ);
-    /*
-    const embed = new MessageEmbed()
-        
-        .setColor('#0099ff')
-        .setTitle(`:arrow_forward: ${np}`)
-        .setDescription(`${showQ}`);
+    } else {
 
-    //return msg.reply(np);
-    msg.channel.send(embed);
-    */
+        let currentPage = 0;
+        const embeds = generateQueue(queue.songs)
+        const queueEmbed = await msg.channel.send(`Pagina atual: ${currentPage + 1}/${embeds.length}`, embeds[currentPage]);
+        await queueEmbed.react('⬅️');
+        await queueEmbed.react('➡️');
+        await queueEmbed.react('❌');
+
+        const filter = (reaction, user) => ['⬅️', '➡️', '❌'].includes(reaction.emoji.name) && (msg.author.id === user.id);
+        const collector = queueEmbed.createReactionCollector(filter)
+
+        collector.on('collect', async (reaction, user) => {
+            if (reaction.emoji.name === '➡️') {
+                if (currentPage < embeds.length - 1) {
+                    currentPage++;
+                    queueEmbed.edit(`Pagina atual: ${currentPage + 1}/${embeds.length}`, embeds[currentPage]);
+                }
+            } else if (reaction.emoji.name === '⬅️') {
+                if (currentPage !== 0) {
+                    --currentPage;
+                    queueEmbed.edit(`Pagina atual ${currentPage + 1}/${embeds.length}`, embeds[currentPage]);
+                }
+            } else {
+                collector.stop();
+                console.log('Parou de coletar')
+                await queueEmbed.delete();
+            }
+        });
+    }
 };
+
+function generateQueue(queue) {
+    const embeds = [];
+
+    let k = 10;
+    for (let i = 0; i < queue.length; i += 10) {
+        const current = queue.slice(i, k);
+        let j = i;
+        k += 10;
+        const info = current.map(queue => `${++j}) [${queue.title}](${queue.url})`).join('\n');
+        const embed = new MessageEmbed()
+            .setAuthor("Tocando agora: ",
+                "https://media.giphy.com/media/ccu9c0Iu7aGFa/giphy.gif")
+            .setDescription(`**${queue[0].title}]**\n\n${info}`)
+        embeds.push(embed);
+    }
+    return embeds;
+}
 
 module.exports = {
     name: "queue",
