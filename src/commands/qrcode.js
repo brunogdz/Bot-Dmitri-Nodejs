cbf29ebe69e14d7966f599c818c5e361c49405f8
Bruno Gomes de Azevedo <brunog.vix@gmail.com>
2020-07-27 18:35:36 -0300

    qrcode colocado

---
+++ src/commands/qrcode.js
@@ -0,0 +1,29 @@
+const MessageEmbed = require("discord.js").MessageEmbed;
+
+
+async function execute(bot, msg, args) {
+    const s = args.join(" ");
+
+    const validar = isValidURL(s)
+    if (validar == true) {
+        const linkNovo = `${linkB}=${encodeURI(s)}`
+        const embed = new MessageEmbed()
+            .setTitle("QR-CODE do link meu consagrado",
+            "https://media.giphy.com/media/ZZrnJf8MDCLPquEVu3/giphy.gif")
+            .setImage(linkNovo)
+        msg.channel.send(embed);
+    } else {
+        msg.reply("Ta de brinqueixon with me cara? Manda um link pessoinha")
+    }
+}
+function isValidURL(string) {
+    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (res !== null)
+};
+
+
+module.exports = {
+    name: "qrcode",
+    help: "Mande um link e ele gera um QR-CODE para você clicar :)",
+    execute,
+}
