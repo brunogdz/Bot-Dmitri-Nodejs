53bcc1c688226a4e48182d4b4b3df4ee7e5ef26c
Bruno Gomes de Azevedo <brunog.vix@gmail.com>
2020-08-19 14:50:30 -0300

    testar sem printar

--- src/commands/testar.js
+++ src/commands/testar.js
@@ -7,9 +7,9 @@ const execute = async (bot, message, args) => {
         url = `https://instagram.com/${args[0]}/?__a=1`;
         response = await axios.get(url)
         account = response.data
-        message.channel.send(account)
+        message.channel.send(`${account}`)
         details = account.graphql.user
-        
+        message.channel.send(`${details}`)
 
 
         const embed = new MessageEmbed()
