5a301214fca00af25fa4c09e993badda4a21b0c5
Bruno Gomes de Azevedo <brunog.vix@gmail.com>
2020-08-19 16:56:48 -0300

    alterei o testar

--- src/commands/testar.js
+++ src/commands/testar.js
@@ -3,44 +3,72 @@ const { MessageEmbed } = require('discord.js');
 
 const execute = async (bot, message, args) => {
     let url, response, account, details;
-    // try {
+    try {
         url = `https://instagram.com/${args[0]}/?__a=1`;
         response = await axios.get(url)
-        message.channel.send(`${response}`)
         account = response.data
-        message.channel.send(`${account}`)
+        // message.channel.send(`${account}`)
         details = account.graphql.user
-        message.channel.send(`${details}`)
+        // message.channel.send(`${details}`)
+        const a = args.join(" ");
+        const dadosAPI = await buscaDadosAPI(a, message);
 
 
         const embed = new MessageEmbed()
-            .setTitle(`${details.is_verified ? `${details.username} <a:verified:727820439497211994>` : ` ${details.username}`} ${details.is_private ? '🔒' : ''} `)
-            .setDescription(details.biography)
-            .setThumbnail(details.profile_pic_url)
+            .setTitle(`${dadosAPI.Verified ? `${dadosAPI.UserName} <a:verified:727820439497211994>` : ` ${dadosAPI.UserName}`} ${dadosAPI.Private ? '🔒' : ''} `)
+            .setDescription(dadosAPI.Biography)
+            .setThumbnail(dadosAPI.ProfilePic)
             .addFields(
                 {
                     name: "Publicações",
-                    value: details.edge_owner_to_timeline_media.count.toLocaleString(),
+                    value: dadosAPI.Publicacoes.toLocaleString(),
                     inline: true
                 },
                 {
                     name: "Seguidores",
-                    value: details.edge_followed_by.count.toLocaleString(),
+                    value: dadosAPI.Seguidores.toLocaleString(),
                     inline: true
                 },
                 {
                     name: "Seguindo",
-                    value: details.edge_follow.count.toLocaleString(),
+                    value: dadosAPI.Seguindo.toLocaleString(),
                     inline: true
                 }
             )
         await message.channel.send(embed)
-    // } catch (error) {
+    } catch (error) {
 
-        // return message.channel.send(`${account}`)
-    // }
+        return message.channel.send(`${account}`)
+    }
 
 }
+const buscaDadosAPI = async (a, message) => {
+    try {
+        var url = `https://instagram.com/${encodeURI(a)}/?__a=1`;
+        const resultado = await axios.get(url);
+        // console.log(resultado)
+        console.log(resultado.data.graphql.user)
+        const { is_verified: Verified, username: UserName, is_private: Private, biography: Biography, profile_pic_url: ProfilePic } = resultado.data.graphql.user;
+        const { count: Publicacoes } = resultado.data.graphql.user.edge_owner_to_timeline_media;
+        const { count: Seguidores } = resultado.data.graphql.user.edge_followed_by;
+        const { count: Seguindo } = resultado.data.graphql.user.edge_follow;
+        return {
+            Verified,
+            UserName,
+            Private,
+            Biography,
+            ProfilePic,
+            Publicacoes,
+            Seguidores,
+            Seguindo
+        }
+
+
+    } catch (erro) {
+        console.log(erro);
+    }
+}
+
 
 module.exports = {
     name: "testar",
