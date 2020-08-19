3108e4af1ae86e73667094fd4e2fe780ae4163be
Bruno Gomes de Azevedo <brunog.vix@gmail.com>
2020-08-19 17:33:52 -0300

    vamos tentar usar o testar com o .data

--- src/commands/testar.js
+++ src/commands/testar.js
@@ -46,7 +46,7 @@ const buscaDadosAPI = async (a) => {
     try {
         var url = `https://instagram.com/${encodeURI(a)}/?__a=1`;
         const resultado = await axios.get(url);
-        console.log(resultado)
+        console.log(resultado.data)
         const { is_verified: Verified, username: UserName, is_private: Private, biography: Biography, profile_pic_url: ProfilePic } = resultado.data.graphql.user;
         const { count: Publicacoes } = resultado.data.graphql.user.edge_owner_to_timeline_media;
         const { count: Seguidores } = resultado.data.graphql.user.edge_followed_by;
