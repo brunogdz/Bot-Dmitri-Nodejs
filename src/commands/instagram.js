98492f44b8ff0b336df2138b5869dd7b7b92ae43
Bruno Gomes de Azevedo <brunog.vix@gmail.com>
2020-08-19 01:35:47 -0300

    alterei denovo

--- src/commands/instagram.js
+++ src/commands/instagram.js
@@ -8,34 +8,34 @@ const execute = async (bot, message, args) => {
         response = await axios.get(url)
         account = response.data
         details = account.graphql.user
+
+
+        const embed = new MessageEmbed()
+            .setTitle(`${details.is_verified ? `${details.username} <a:verified:727820439497211994>` : ` ${details.username}`} ${details.is_private ? '🔒' : ''} `)
+            .setDescription(details.biography)
+            .setThumbnail(details.profile_pic_url)
+            .addFields(
+                {
+                    name: "Publicações",
+                    value: details.edge_owner_to_timeline_media.count.toLocaleString(),
+                    inline: true
+                },
+                {
+                    name: "Seguidores",
+                    value: details.edge_followed_by.count.toLocaleString(),
+                    inline: true
+                },
+                {
+                    name: "Seguindo",
+                    value: details.edge_follow.count.toLocaleString(),
+                    inline: true
+                }
+            )
+        await message.channel.send(embed)
     } catch (error) {
-        
+
         return message.channel.send(`${error}`)
     }
-
-    const embed = new MessageEmbed()
-        .setTitle(`${details.is_verified ? `${details.username} <a:verified:727820439497211994>` : ` ${details.username}`} ${details.is_private ? '🔒' : ''} `)
-        .setDescription(details.biography)
-        .setThumbnail(details.profile_pic_url)
-        .addFields(
-            {
-                name: "Publicações",
-                value: details.edge_owner_to_timeline_media.count.toLocaleString(),
-                inline: true
-            },
-            {
-                name: "Seguidores",
-                value: details.edge_followed_by.count.toLocaleString(),
-                inline: true
-            },
-            {
-                name: "Seguindo",
-                value: details.edge_follow.count.toLocaleString(),
-                inline: true
-            }
-        )
-    await message.channel.send(embed)
-
 
 }
 
